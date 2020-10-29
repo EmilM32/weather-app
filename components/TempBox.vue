10e284eca35936480b24e131c339efcbd613f104
Emil Majer <emajer32@gmail.com>
2020-10-29 14:44:17 +0100

    Added temperature box

---
+++ components/TempBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="temp-box" :class="{ 'small-box': small }">
+    <v-row v-if="small" justify="center">
+      <span class="temp-title">{{ title }}</span>
+    </v-row>
+    <v-row no-gutters justify="space-between" align="center">
+      <span class="temp-value">
+        {{ temperature }}
+      </span>
+      <v-icon :style="{ fontSize: `${small ? 3 : 4}rem` }"
+        >mdi-temperature-celsius</v-icon
+      >
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "nuxt-property-decorator";
+
+@Component
+export default class TempBox extends Vue {
+  @Prop() readonly temperature!: number;
+  @Prop({ default: false }) readonly small!: boolean;
+  @Prop({ default: "" }) readonly title!: string;
+}
+</script>
+<style lang="sass" scoped>
+.temp-box
+  font-family: "Montserrat"!important
+  display: inline
+  margin: 1rem
+  font-size: 3.5rem
+
+.small-box
+  font-size: 2.5rem
+  opacity: .7
+
+.temp-title
+  font-size: 1rem
+
+.temp-value
+  font-weight: bold
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,58 @@
 <template>
-  <div>
-    <v-card>
-      <v-card-text>{{ $t(dates.dayTranslation) }} {{ $t(dates.monthTranslation) }} {{ dates.dateOfMonth }}</v-card-text>
-      <v-btn @click="clickMe('pl')">pl</v-btn>
-      <v-btn @click="clickMe('en')">en</v-btn>
-    </v-card>
-    <!-- test -->
+  <div class="d-flex justify-center">
+    <v-row no-gutters>
+      <v-col align="center" cols="12">
+        <span class="display-2 day">{{ $t(dates.dayTranslation) }}</span>
+      </v-col>
+      <v-col align="center" cols="12">
+        <span class="subtitle-2 date">
+          {{ $t(dates.monthTranslation) }} {{ dates.dateOfMonth }}
+        </span>
+      </v-col>
+      <v-col>
+        <v-row align-content="end">
+          <v-spacer />
+          <TempBox
+            :temperature="5"
+            :small="true"
+            title="MIN"
+          />
+          <TempBox :temperature="15" />
+          <TempBox
+            :temperature="5"
+            :small="true"
+            title="MAX"
+          />
+          <v-spacer />
+        </v-row>
+      </v-col>
+      <v-col align="center" cols="12">
+        <v-icon style="font-size: 15rem">mdi-weather-cloudy</v-icon>
+      </v-col>
+    </v-row>
+    <!-- <v-btn @click="clickMe('pl')">pl</v-btn>
+      <v-btn @click="clickMe('en')">en</v-btn> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "nuxt-property-decorator";
-import { Dates } from "@/logic/dates"
-
+import { Dates } from "@/logic/dates";
+import TempBox from "@/components/TempBox.vue";
 
 @Component
 export default class Main extends Vue {
-  dates= new Dates('days', 'months')
+  dates = new Dates("days", "months");
 
   clickMe(locale: string): void {
     this.$i18n.setLocale(locale);
   }
 }
 </script>
+<style lang="sass" scoped>
+.day
+  font-family: "Montserrat"!important
+
+.date
+  opacity: .7
+</style>
